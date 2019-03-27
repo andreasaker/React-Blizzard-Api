c8a497f83b1026a61a0e0c3629561a910484da4c
unknown <andreas.aker91@gmail.com>
2019-03-27 17:20:31 +0100

    Init git

--- src/App.js
+++ src/App.js
@@ -1,73 +1,27 @@
-import React, {useState} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {getToken, getCharacterProfile} from './api/Battlenet';
 
+const App = () => {
 
-const App = params => {
-  let cors = require('cors');
-  const axios = require('axios');
-  let apiToken = {};
-
- 
-
-  const tokenRequest = () => {
-    
-    axios.get(`https://eu.battle.net/oauth/token`, {
-        auth: {
-          username: process.env.REACT_APP_BNET_ID,
-          password: process.env.REACT_APP_BNET_SECRET,
-        },
-        params: {
-          grant_type: 'client_credentials',
-        }
-    }).then(function (response) {
-      console.log(response.data);
-      apiToken = response;
-    }).catch(function (error) {
-      console.log(error);
-    });
-    
-  }
-
- 
-  
-  let getCharacterProfile = (region, realm, characterName) => {
-    let href = 'https://' + region + '.battle.net/wow/character/' +  realm + '/' + characterName
- 
-    axios.get(href, {
-        headers: {
-          Authorization: 'Bearer ' + apiToken,
-          'Access-Control-Allow-Origin': "https://shrouded-wave-90291.herokuapp.com",
-          'Access-Control-Allow-Credentials': true
-        }
-    }).then(function (response) {
-      //apiToken = response;
-      console.log(response);
-    }).catch(function (error) {
-      console.log(error);
-    });
-  }
-
-  
-
-
+   
+   
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            
           </p>
-          <a href="#" onClick={tokenRequest}>
-            request token
-          </a>
-          <a href="#" onClick={() => getCharacterProfile("eu", "Quel'Thalas", "Portvakt")}>
+          <a href="#" onClick={getCharacterProfile("eu", "Quel'Thalas", "Portvakt")}>
             request character
           </a>
+          
         </header>
       </div>
     );
   
 }
-
+//"eu", "Quel'Thalas", "Portvakt"
 export default App;
---
+++ src/api/Battlenet.js
@@ -0,0 +1,42 @@
+
+
+
+
+  const axios = require('axios');
+  let token = {}
+
+
+  export let getToken = () => {
+    axios.get(`https://eu.battle.net/oauth/token`, {
+        auth: {
+          username: process.env.REACT_APP_BNET_ID,
+          password: process.env.REACT_APP_BNET_SECRET,
+        },
+        params: {
+          grant_type: 'client_credentials',
+        }
+    }).then(function (response) {
+      console.log(response);
+      token = response;
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }
+
+
+ 
+  
+  export let getCharacterProfile = (region, realm, characterName) => {
+    let href = 'https://cors-anywhere.herokuapp.com/https://' + region + '.api.blizzard.com/wow/character/' +  realm + '/' + characterName + '?fields=appearance&access_token=' + token.data.access_token
+    
+    axios.get(href, {
+        headers: {
+          Authorization: 'Bearer ' + token.data.access_token
+        }
+    }).then(function (response) {
+      console.log(response);
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }
+
